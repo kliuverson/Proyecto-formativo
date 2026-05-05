e09ca95100b03f324fa5b167d52896d81284df77
mariacamacho10 <mariaangelcamacho4@gmail.com>
2026-05-05 01:44:19 -0500

    Implementa navegación de categorias a productos

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -1,18 +1,20 @@
-import 'package:ferremateriales/view/modulos/admin/page/admin_page.dart';
-import 'package:ferremateriales/view/modulos/modules/orders/pages/orders_pages.dart';
-import 'package:ferremateriales/view/modulos/productos/pages/products_list_page.dart';
-import 'package:ferremateriales/view/modulos/profile/cubit/profile_cubit.dart';
-import 'package:ferremateriales/view/modulos/profile/models/profile_user_model.dart';
-import 'package:ferremateriales/view/modulos/profile/pages/edit_profile_page.dart';
-import 'package:ferremateriales/view/modulos/register/register.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ferremateriales/view/home/page/home_page.dart';
 import 'package:ferremateriales/view/login/login.dart';
-import 'package:ferremateriales/view/modulos/category/pages/category_page.dart';
 import 'package:ferremateriales/view/modulos/carrito/pages/cart_page.dart';
-import 'package:ferremateriales/view/modulos/profile/pages/profile_page.dart';
+import 'package:ferremateriales/view/modulos/category/pages/category_page.dart';
 import 'package:ferremateriales/view/modulos/favorites/pages/favorite_page.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ferremateriales/view/modulos/modules/orders/pages/orders_pages.dart';
+import 'package:ferremateriales/view/modulos/productos/pages/products_list_page.dart';
+import 'package:ferremateriales/view/modulos/productos/pages/products_by_category_page.dart';
+import 'package:ferremateriales/view/modulos/admin/page/admin_page.dart';
+import 'package:ferremateriales/view/modulos/profile/pages/profile_page.dart';
+import 'package:ferremateriales/view/modulos/profile/pages/edit_profile_page.dart';
+import 'package:ferremateriales/view/modulos/profile/cubit/profile_cubit.dart';
+import 'package:ferremateriales/view/modulos/profile/models/profile_user_model.dart';
+import 'package:ferremateriales/view/modulos/register/register.dart';
+import 'package:ferremateriales/view/modulos/category/cubit/category_product_cubit.dart';
 
 class AppRoutes {
   static const home = '/home';
@@ -26,12 +28,17 @@ class AppRoutes {
   static const products = '/products';
   static const admin = '/admin';
   static const editProfile = '/edit-profile';
+  static const productsByCategory = '/productsByCategory';
 
   static Map<String, WidgetBuilder> routes = {
     home: (context) => const HomePage(),
+
     login: (context) => const Login(),
+
     cart: (context) => const CartPage(),
+
     category: (context) => const CategoryPage(),
+
     profile:
         (context) => BlocProvider.value(
           value: context.read<ProfileCubit>()..getUserProfile(),
@@ -39,14 +46,36 @@ class AppRoutes {
         ),
 
     editProfile: (context) {
-      final user = ModalRoute.of(context)!.settings.arguments as UserProfileModel;
+      final user =
+          ModalRoute.of(context)!.settings.arguments as UserProfileModel;
       return EditProfilePage(user: user);
     },
-    
+
     favorite: (context) => const FavoritesPage(),
+
     orders: (context) => OrdersPage(),
+
     register: (context) => const RegisterScreen(),
+
     products: (context) => const ProductListPage(),
+
+    productsByCategory: (context) {
+      final args = ModalRoute.of(context)!.settings.arguments;
+
+      if (args == null || args is! String) {
+
+        return const CategoryPage();
+      }
+
+      final categoryName = args.toString();
+
+      return BlocProvider(
+        create:
+            (_) => CategoryProductCubit()..loadProductsByCategory(categoryName),
+        child: ProductsByCategoryPage(categoryName: categoryName),
+      );
+    },
+
     admin: (context) => const CreateProductPage(),
   };
 }
